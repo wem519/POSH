22958f5eff71b3afdfd9895c8e69009214a54cf4
wem519 <wem519@gamail.com>
2021-11-09 12:24:48 +0900

    Design: login & signup

--- src/components/units/accounts/login/Login.presenter.tsx
+++ src/components/units/accounts/login/Login.presenter.tsx
@@ -1,88 +1,35 @@
-import { TitleWrapper, Wrapper } from "./Login.styles";
+import {
+  TitleWrapper,
+  Wrapper,
+  Title,
+  AccountWrapper,
+  Account,
+  AccountInfo,
+  Error,
+  ButtonWrapper,
+  Login,
+} from "./Login.styles";
 
 export default function LoginUI() {
   return (
     <>
       <Wrapper>
         <TitleWrapper>
-          {/* <img
-            src="/images/logo.png"
-            style={{
-              width: "154px",
-              height: "87px",
-              fontSize: "64px",
-            }}
-          /> */}
-          <div
-            style={{
-              fontSize: "64px",
-              color: "#FFFFFF",
-              fontFamily: "NotoSansitalic",
-              lineHeight: "87px",
-              fontWeight: "bold",
-            }}
-          >
-            POSH
-          </div>
+          <Title>POSH</Title>
         </TitleWrapper>
-        <div
-          style={{
-            marginTop: "34px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div style={{ marginTop: "38px" }}>
-            <input
-              placeholder="email ID"
-              style={{
-                width: "358px",
-                height: "52px",
-                paddingLeft: "24px",
-                backgroundColor: "rgba(249,249,249,0.8)",
-                borderRadius: "4px",
-                border: "0px solid rgba(249,249,249,0.8) ",
-                fontSize: "16px",
-              }}
-            />
-            <div></div>
-          </div>
-          <div style={{ marginTop: "38px" }}>
-            <input
-              placeholder="Password"
-              style={{
-                width: "358px",
-                height: "52px",
-                paddingLeft: "24px",
-                backgroundColor: "rgba(249,249,249,0.8)",
-                borderRadius: "4px",
-                border: "0px solid rgba(249,249,249,0.8) ",
-                fontSize: "16px",
-              }}
-            />
-            <div></div>
-          </div>
-          <div style={{ marginTop: "81px" }}>
-            <button
-              style={{
-                width: "358px",
-                height: "54px",
-                color: "#8915A6",
-                fontFamily: "NotoSansKRmedium",
-                fontWeight: 700,
-                fontSize: "18px",
-                lineHeight: "26px",
-                borderRadius: "4px",
-                backgroundColor: "#FFFFFF",
-                border: "0px solid #FFFFFF ",
-              }}
-            >
-              로그인
-            </button>
-          </div>
-        </div>
+        <AccountWrapper>
+          <Account>
+            <AccountInfo placeholder="email ID" type="text" />
+            <Error></Error>
+          </Account>
+          <Account>
+            <AccountInfo placeholder="Password" type="password" />
+            <Error></Error>
+          </Account>
+          <ButtonWrapper>
+            <Login>로그인</Login>
+          </ButtonWrapper>
+        </AccountWrapper>
       </Wrapper>
     </>
   );
--- src/components/units/accounts/signup/Signup.presenter.tsx
+++ src/components/units/accounts/signup/Signup.presenter.tsx
@@ -1,136 +1,60 @@
+import {
+  Wrapper,
+  ProfileWrapper,
+  Profile,
+  AccountWrapper,
+  AccountInfo,
+  Label,
+  AccountInput,
+  SubmitWrapper,
+  Signup,
+} from "./Signup.styles";
+
 export default function SignupUI() {
   return (
-    <div
-      style={{
-        fontFamily: "NotoSansKRregular",
-        width: "390px",
-        height: "844px",
-        backgroundColor: "#8915A6",
-      }}
-    >
+    <Wrapper>
       <div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            paddingTop: "75px",
-          }}
-        >
-          <div
-            style={{
-              borderRadius: "50%",
-              backgroundColor: "#f1f1f1",
-              width: "118px",
-              height: "118px",
-              paddingRight: "10px",
-              display: "flex",
-              //   flexDirection: "row",
-              justifyContent: "center",
-              textAlign: "center",
-              alignItems: "center",
-              color: "#8915A6",
-              fontFamily: "NotoSansregular",
-              fontStyle: "italic",
-              fontSize: "64px",
-            }}
-          >
-            P
-          </div>
-        </div>
-        <div
-          style={{
-            paddingTop: "30px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div style={{ paddingTop: "30px" }}>
-            <div style={{ color: "#FFFFFF" }}>이메일*</div>
-            <input
-              style={{
-                width: "358px",
-                height: "52px",
-                marginTop: "9px",
-                paddingLeft: "24px",
-                borderRadius: "4px",
-                border: "0px solid #FFFFFF",
-              }}
+        <ProfileWrapper>
+          <Profile>P</Profile>
+        </ProfileWrapper>
+        <AccountWrapper>
+          <AccountInfo>
+            <Label>이메일*</Label>
+            <AccountInput
+              type="text"
               placeholder="이메일을 입력해주세요"
-            ></input>
+            ></AccountInput>
             <div></div>
-          </div>
-          <div style={{ paddingTop: "39px" }}>
-            <div style={{ color: "#FFFFFF" }}>아이디*</div>
-            <input
-              style={{
-                width: "358px",
-                height: "52px",
-                marginTop: "9px",
-                paddingLeft: "24px",
-                borderRadius: "4px",
-                border: "0px solid #FFFFFF",
-              }}
+          </AccountInfo>
+          <AccountInfo>
+            <Label>아이디*</Label>
+            <AccountInput
+              type="text"
               placeholder="아이디를 입력해주세요"
-            ></input>
+            ></AccountInput>
             <div></div>
-          </div>
-          <div style={{ paddingTop: "39px" }}>
-            <div style={{ color: "#FFFFFF" }}>비밀번호*</div>
-            <input
-              style={{
-                width: "358px",
-                height: "52px",
-                marginTop: "9px",
-                paddingLeft: "24px",
-                borderRadius: "4px",
-                border: "0px solid #FFFFFF",
-              }}
+          </AccountInfo>
+          <AccountInfo>
+            <Label>비밀번호*</Label>
+            <AccountInput
+              type="password"
               placeholder="비밀번호를 입력해주세요"
-            ></input>
+            ></AccountInput>
             <div></div>
-          </div>
-          <div style={{ paddingTop: "39px" }}>
-            <div style={{ color: "#FFFFFF" }}>비밀번호 확인*</div>
-            <input
-              style={{
-                width: "358px",
-                height: "52px",
-                marginTop: "9px",
-                paddingLeft: "24px",
-                borderRadius: "4px",
-                border: "0px solid #FFFFFF",
-              }}
+          </AccountInfo>
+          <AccountInfo>
+            <Label>비밀번호 확인*</Label>
+            <AccountInput
+              type="password"
               placeholder="비밀번호를 입력해주세요"
-            ></input>
+            ></AccountInput>
             <div></div>
-          </div>
-        </div>
-        <div
-          style={{
-            paddingTop: "50px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <button
-            style={{
-              width: "358px",
-              height: "54px",
-              color: "#8915A6",
-              borderRadius: "4px",
-              border: "0px solid #FFFFFF",
-            }}
-          >
-            시작하기
-          </button>
-        </div>
+          </AccountInfo>
+        </AccountWrapper>
+        <SubmitWrapper>
+          <Signup>시작하기</Signup>
+        </SubmitWrapper>
       </div>
-    </div>
+    </Wrapper>
   );
 }
